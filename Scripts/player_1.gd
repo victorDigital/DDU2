cf89a3e905ab69a54261359fb33a50813cabc6b9
August "Agpvpz <87880073+Agpvpz@users.noreply.github.com>
2024-09-23 20:09:31 +0200

    start tilesets/tilemap
    skal dog rettes noget gevaldigt

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 450.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta*1.5
 	# Handle jump.
 	if Input.is_action_just_pressed("player1_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
