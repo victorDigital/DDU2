3cd62029792e65eff8f28a7788cfc4c77894c669
ArcticHere <seiergustav@gmail.com>
2024-10-09 12:44:13 +0200

    smol health pickup change :alien:

--- Scripts/health_pickup.gd
+++ Scripts/health_pickup.gd
@@ -8,7 +8,7 @@ var pickup_effect = preload("res://scenes/pickup_death_effect.tscn")
 func _on_body_entered(body: Node2D) -> void:
 	print("health")
 	if body.has_method("increase_health"):
-		body.increase_health(20)
+		body.increase_health(50)
 		var pickup_effect_instance = pickup_effect.instantiate() as Node2D
 		pickup_effect_instance.global_position = global_position
 		get_parent().add_child(pickup_effect_instance)
